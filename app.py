c4c51a00392090c11c2bd08c92fdd5cf9fd70b05
edeliversolutions <74201180+edeliversolutions@users.noreply.github.com>
2023-10-24 12:43:17 +0000

    JOSON

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify 
 
 app = Flask(__name__)
 
+JOBS = [
+    {"id": 1, "title": "Python Developer", "location": "New York", "salary": "$100,000"},
+    {"id": 2, "title": "Web Developer", "location": "San Francisco", "salary": "$120,000"},
+    {"id": 3, "title": "Data Scientist", "location": "Chicago"}
+]  
 
+
+    
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
-
+  return render_template('home.html',
+                         jobs=JOBS,
+                         company_name= "Jovian")
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
